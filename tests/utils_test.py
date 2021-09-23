7a32ec73e6556567f41940947c1e0e48df6c2cc3
Aurélien Chick <aurelien.chick@gmail.com>
2021-09-24 00:29:28 +0200

    Update utils test.
    Signed-off-by: Aurélien Chick <aurelien.chick@gmail.com>

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -34,3 +34,23 @@ def test_read_resources(tmpdir):
 
     with pytest.raises(InvalidResourceFile):
         utils.read_resources(pat_path, QueryLevel.STUDY)
+
+
+@pytest.mark.cli
+def test_is_db_uri():
+    """Test that database URIs are correctly picked up."""
+    db_uris = [
+        "postgresql://foo:bar@localhost/mydatabase",
+        "postgresql+psycopg2://foo:bar@localhost/mydatabase",
+        "postgresql+pg8000://foo:bar@localhost/mydatabase",
+        "mysql://foo:bar@localhost/foo",
+        "mysql+mysqldb://foo:bar@localhost/foo",
+        "oracle://foo:bar@127.0.0.1:1521/mydb",
+        "sqlite:///foo.db",
+    ]
+    for uri in db_uris:
+        assert utils.is_db_uri(uri) is True
+
+    non_db_uris = ["/data/foo/bar.json", "sqlite.csv"]
+    for uri in non_db_uris:
+        assert utils.is_db_uri(uri) is False
