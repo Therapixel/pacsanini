0940b8c09f1adabbf6ea574f7dcd859b65c296fe
Aurélien Chick <aurelien.chick@gmail.com>
2021-10-02 12:14:06 +0200

    Added dump command for CLI. see #63
    Signed-off-by: Aurélien Chick <aurelien.chick@gmail.com>

--- src/pacsanini/cli/db.py
+++ src/pacsanini/cli/db.py
@@ -3,12 +3,19 @@
 # This file is subject to the terms and conditions described in the
 # LICENSE file distributed in this package.
 """Database utility commands that are used from the command line."""
+from typing import List
+
 import click
 
 from sqlalchemy import create_engine
 
 from pacsanini.config import PacsaniniConfig
-from pacsanini.db.utils import initialize_database
+from pacsanini.db.utils import (
+    TABLES,
+    dump_database,
+    get_db_session,
+    initialize_database,
+)
 
 
 @click.command(name="init")
@@ -39,9 +46,45 @@ def init_cli(config: str, force_init: bool):
     initialize_database(engine, force_init=force_init)
 
 
+@click.command(name="dump")
+@click.option(
+    "-f",
+    "--config",
+    required=True,
+    type=click.Path(exists=True),
+    help="The path to the configuration file to use for dumping the database.",
+)
+@click.option(
+    "-o",
+    "--output",
+    default=None,
+    help="If set, specify the output directory to write results to. They will be written to the current directory otherwise.",
+)
+@click.option(
+    "-t",
+    "--table",
+    type=click.Choice(TABLES.keys()),
+    multiple=True,
+    show_choices=True,
+    default=list(TABLES.keys()),
+    help="If specified, select one or more tables to dump in CSV format. The default is all tables.",
+)
+def dump_cli(config: str, output: str, table: List[str]):
+    """Dump pacsanini database tables in CSV format."""
+    ext = config.rsplit(".", 1)[-1].lower()
+    load_func = (
+        PacsaniniConfig.from_json if ext == "json" else PacsaniniConfig.from_yaml
+    )
+    pacsanini_config = load_func(config)
+
+    with get_db_session(pacsanini_config.storage.resources) as session:
+        dump_database(session, output=output, tables=table)
+
+
 @click.group(name="db")
 def db_cli_group():
     """Perform database related commands."""
 
 
 db_cli_group.add_command(init_cli)
+db_cli_group.add_command(dump_cli)
